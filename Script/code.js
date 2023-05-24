6ae1ba58f3f14f8fd7d0adc4c36fc0b0f0c5a4dd
Lithiniskhikhi <lithiniskhikhi9@gmail.com>
2023-05-24 16:39:32 +0200

    commit

--- Script/code.js
+++ Script/code.js
@@ -9,3 +9,21 @@ function fullName (){
 
 }
 console.log(fullName());
+
+function age (){
+    let myAge = "19";
+    return myAge 
+}
+console.log(age());
+
+function subjects(){
+    let array =["eng","math","afrikaans"]
+    return array
+}
+console.log(subjects());
+
+function living(){
+    let myAddress =["Mowbray","Montreal","two street"]
+    return myAddress
+}
+console.log(living());
